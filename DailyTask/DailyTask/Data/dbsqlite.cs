c4cfdab8670098c3096db5268f29407abb0507b9
deveiap <85376092+deveiap@users.noreply.github.com>
2022-10-02 20:10:35 -0500

    Agregar archivos de proyecto.

---
+++ DailyTask/DailyTask/Data/dbsqlite.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DailyTask.Models;
+using SQLite;
+
+namespace DailyTask.Data
+{
+    public class dbsqlite
+    {
+        string dbruta = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+
+        //CRUD TAREAS
+        public bool conectdb()
+        {
+            try
+            {
+                using (var cox = new SQLiteConnection(System.IO.Path.Combine(dbruta, "tarea.db")))
+                {
+                    cox.CreateTable<TareaModel>();
+                    return true;
+                }
+            }
+            catch (SQLiteException)
+            {
+                //Log.Info("No SQLite", ex.Message);
+                return false;
+            }
+        }
+        public bool Agregar(TareaModel tareaModel)
+        {
+            try
+            {
+                using (var cox = new SQLiteConnection(System.IO.Path.Combine(dbruta, "tarea.db")))
+                {
+                    cox.Insert(tareaModel);
+                    return true;
+                }
+            }
+            catch (SQLiteException)
+            {
+                //Log.Info("No se inserto la tarea", ex.Message);
+                return false;
+            }
+        }
+        public List<TareaModel> GetTareaModels()
+        {
+
+            try
+            {
+                using (var cox = new SQLiteConnection(System.IO.Path.Combine(dbruta, "tarea.db")))
+                {
+                    return cox.Table<TareaModel>().ToList();
+                }
+            }
+            catch (SQLiteException)
+            {
+                //Log.Info("No se pueden ver las tareas", ex.Message);
+                return null;
+            }
+        }
+        public TareaModel GetTareaUnica(int id)
+        {
+            using(var cox = new SQLiteConnection(System.IO.Path.Combine(dbruta, "tarea.db")))
+            {
+                TareaModel TU = (from mem in cox.Table<TareaModel>() where mem.ID== id select mem).FirstOrDefault();
+                return TU;
+            }
+        }
+        public bool Actualizar(TareaModel tareaModel)
+        {
+            try
+            {
+                using (var cox = new SQLiteConnection(System.IO.Path.Combine(dbruta, "tarea.db")))
+                {
+                    cox.Query<TareaModel>("UPDATE TareaModel set NombreTarea=?, Categoria=?, Descripcion=?, Fecha=? Where ID=?", tareaModel.NombreTarea, tareaModel.Categoria, tareaModel.Descripcion, tareaModel.Fecha, tareaModel.ID);
+                    return true;
+                }
+            }
+            catch (SQLiteException)
+            {
+                //Log.Info("No se actualizo la tarea", ex.Message);
+                return false;
+            }
+        }
+        public bool Eliminar(TareaModel tareaModel)
+        {
+            try
+            {
+                using (var cox = new SQLiteConnection(System.IO.Path.Combine(dbruta, "tarea.db")))
+                {
+                    cox.Delete(tareaModel);
+                    return true;
+                }
+            }
+            catch (SQLiteException)
+            {
+                //Log.Info("No se elimino la tarea", ex.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ DailyTask/DailyTask/Models/TareaModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace DailyTask.Models
+{
+    public class TareaModel
+    {
+        public enum listac { estudio, personal, trabajo, otros }
+        [PrimaryKey, AutoIncrement]
+
+        public int ID { get; set; }
+
+        public string NombreTarea { get; set; }
+
+        public listac Categoria { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public DateTime Fecha { get; set; }
+    }
+}
